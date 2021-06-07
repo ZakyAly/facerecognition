b200c5c295d3e30abeb4b4cc5e32b11883a91d94
zaky aly <zaky3ly@gmail.com>
2021-06-07 14:47:21 +0200

    create Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p className="f5 link dim black pa3 underline pointer">Sign Out</p>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
