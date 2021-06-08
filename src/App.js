b6f227c947f17a4cc8aa384b097702a388e9118c
zaky aly <zaky3ly@gmail.com>
2021-06-08 08:24:13 +0200

    import Rank, ImageLinkForm components and Particles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
 
+import Particles from "react-particles-js";
+
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank.js";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm /> */}
+      <Rank />
+      <ImageLinkForm />
       {/* <FaceRecognition /> */}
+      <Particles params={particlesOptions} />
     </div>
   );
 }
