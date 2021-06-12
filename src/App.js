d172c240c15f8baa3c2fa632ada4332490ba5a5c
zaky aly <zaky3ly@gmail.com>
2021-06-12 11:54:23 +0200

    importing Clarifai, updating particles, adding onInputChange and onSubmit functions

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import "./App.css";
 
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank.js";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 
 const particlesOptions = {
   particles: {
-    line_linked: {
-      shadow: {
+    number: {
+      value: 20,
+      density: {
         enable: true,
-        color: "#3CA9D1",
-        blur: 5,
+        value_area: 1000,
       },
     },
   },
 };
 
+const app = new Clarifai.App({
+  apiKey: "dc2a8c7fd1ad4e088124cef04dd2a028",
+});
+
 function App() {
+  const onInputChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const onSubmit = () => {
+    console.log("clicked");
+    app.models.predict("").then(
+      function (response) {
+        console.log(response);
+      },
+      function (err) {
+        //
+      }
+    );
+  };
+
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-      <Particles params={particlesOptions} />
+      <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
+      <FaceRecognition />
     </div>
   );
 }
