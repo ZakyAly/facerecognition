db417ac3376f9d6b0648ed1a9d49efed89ef61ba
zaky aly <zaky3ly@gmail.com>
2021-06-12 11:55:38 +0200

    passing onInputChange and onSubmit to imageLinkForm

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 "tachyons"
 "react-tilt"
+"particles.js"
+"clarifai"
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className="f3">{"This is a Magic!"}</p>
       <p>give it a try ..</p>
       <div>
         <div className="pa4 br3 shadow-1 w-80 center">
-          <input type="text" className=" w-80" />
-          <button className="pointer grow link dib white bg-light-purple">
+          <input type="text" className=" w-80" onChange={onInputChange} />
+          <button
+            className="pointer grow link dib white bg-light-purple"
+            onClick={onSubmit}
+          >
             Recognize
           </button>
         </div>
