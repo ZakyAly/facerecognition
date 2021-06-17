9dbe0036bf822d27f4b83480d95c37a726511397
zaky aly <zaky3ly@gmail.com>
2021-06-17 17:20:08 +0200

    CalculateFaceLocation component

---
+++ src/components/CalculateFaceLocation/CalculateFaceLocation.js
@@ -0,0 +1,17 @@
+const CalculateFaceLocation = (data) => {
+  // Todo : try the calc for two or more >> look at data.outputs[]
+
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById("inputimage");
+  const width = Number(image.width);
+  const height = Number(image.height);
+
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - clarifaiFace.right_col * width,
+    bottomRow: height - clarifaiFace.bottom_row * height,
+  };
+};
+
+export default CalculateFaceLocation;
